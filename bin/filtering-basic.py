Fixed A1 errors on calculation of mitochondrial and counts stats

bin/filtering-basic.py
@@ -59,10 +59,12 @@ def initialFiltering( args ):
     # mito and genes/counts cuts
     mito_genes = adata.var_names.str.startswith('MT-')
     # for each cell compute fraction of counts in mito genes vs. all genes
-    adata.obs['percent_mito'] = np.sum(
-        adata[:, mito_genes].X, axis=1).A1 / np.sum(adata.X, axis=1).A1
+    if( sum(mito_genes)==0 ):
+        adata.obs['percent_mito'] = 0.0
+    else:
+        adata.obs['percent_mito'] = np.ravel(np.sum(np.asmatrix(adata[:, mito_genes].X), axis=1)) / np.ravel(np.sum(adata.X, axis=1))
     # add the total counts per cell as observations-annotation to adata
-    adata.obs['n_counts'] = adata.X.sum(axis=1).A1
+    adata.obs['n_counts'] = np.ravel(adata.X.sum(axis=1))
 
     ####################
     # plotting: